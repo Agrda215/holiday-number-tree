6d41fab625448813a1f19b55d1b2ad1f85d27900
Glitch (gray-volcano-cereal) <none>
2022-07-16 22:19:05 +0000

    update layer

--- js/layers.js
+++ js/layers.js
@@ -1,14 +1,14 @@
-addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("h1", {
+    name: "holiday 1", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "H<sub>1</sub>", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new ExpantaNum(0),
     }},
-    color: "#4BDC13",
+    color: "#FF00FF",
     requires: new ExpantaNum(10), // Can be a function that takes requirement increases into account
-    resource: "prestige points", // Name of prestige currency
+    resource: "holiday1", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
@@ -21,8 +21,5 @@ addLayer("p", {
         return new ExpantaNum(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
     layerShown(){return true}
 })
